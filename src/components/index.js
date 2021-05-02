3ad9b79a13980cc24394034bd1e91d2239b56f6a
Nina Cummings <67253353+jaderiver62@users.noreply.github.com>
2021-05-01 20:40:04 -0500

    Create index.js

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+import About from "./About";
+import Contact from "./Contact";
+import Footer from "./Footer";
+import Header from "./Header";
+import Nav from "./Nav";
+import Portfolio from "./Portfolio";
+import Resume from "./Resume";
+import SingleProject from "./SingleProject";
+
+export { About, Contact, Footer, Header, Nav, Portfolio, Resume, SingleProject};
