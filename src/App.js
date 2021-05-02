cb9cc2c3dfeae94e9d9336459ccd358fd52208af
Nina Jade <jaderiver64@gmail.com>
2021-05-01 21:18:48 -0500

    building component structure

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
     <div>
       <main>
         <Header></Header>
+        <About></About>
       </main>
     </div>
   );
---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Nav from "../Nav";
+/*import coverImage from "../../assets/header/starbirth.jpg";
+*/
+function Header() {
+	return (
+		<section>
+			Header
+			<Nav></Nav>
+{/*			<img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+*/}		</section>
+
+	);
+}
+
+export default Header;
