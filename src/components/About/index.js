2fee8fa5f9f4e4d4e40e540a20637bb1173b074b
Nina Jade <jaderiver64@gmail.com>
2021-05-02 23:38:39 -0500

    building format

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,35 +1,45 @@
 
 import React from 'react';
-import ninaImage from "../../assets/about-me/nina-cummings.jpg";
+import ninaImage from "../../assets/about-me/nina-cummings.png";
 import divider from "../../assets/about-me/divider.png";
+import divider2 from "../../assets/about-me/divider-2.png";
 
 //TODO: Style this
 
 function About() {
 	return (
 		<section class="about">
-			<img src={ninaImage} style={{ /*width: "15%",*/ padding:"60px" }} alt="Nina" class="nina-pic" />
+			<img src={ninaImage} style={{ /*width: "15%",*/ padding: "40px" }} alt="Nina" class="nina-pic" />
 			<div>
-				<h1>About Me</h1>
-				<img src={divider} style={{ width: "10%"}} alt="divider" />
+				<br /><br />
 				<p>
-				Greetings!
+					<h1>About Me</h1>
 
-				I am Nina, a humble Baker by day.  In my spare time, however, I am an eager student of coding.  
-				
-				My goal is to continue to learn more about development every day, and my work will reflect my endeavors.
-				
-				In the broad scheme of things, I am studying computer science and mathematics at the University of Minnesota.  
-				
-				Most recently, I have been attending a coding boot camp cohort at UC Berkeley Extension for Full Stack Web Development.
-
-				I have been coding for several years now, primarily in Java, Python and most recently in JavaScript.   
-				
-				Sincerely, I love to code.
-
-				Watch this space...
-				</p>
-		</div>
+					<img src={divider2} style={{ width: "20%" }} alt="divider" class="divider2" />
+					<br />
+					Greetings!
+					<br />
+					I am Nina, a humble Baker by day.....
+					<br />
+					In my spare time, however, I am an eager student of coding!
+					<br />
+					My primary goal is to continue to learn more about development every day, and my work will reflect my endeavors.
+					<br />
+					<br />
+					In the broad scheme of things, I am studying computer science and mathematics at the University of Minnesota.
+					<br />
+					Most recently, I have been attending a coding boot camp cohort at UC Berkeley Extension for Full Stack Web Development.
+					<br />
+					I have been coding now for several years and have only become more fascinated with time.
+					<br /><br />
+					Sincerely, I love to code!
+					<br /><br />
+					Watch this space...
+				</p><br />
+				<br />
+<img src={divider} style={{ width: "40%" }} alt="divider" class="divider" />
+			</div>
+			
 		</section>
 	);
 }
