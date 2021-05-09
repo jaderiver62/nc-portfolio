83bb872d413c5b8ffb74b4772e6e8691a66f23fb
Nina Cummings <jaderiver64@gmail.com>
2021-05-09 01:53:37 -0500

    font adjustment

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -10,7 +10,7 @@ function Project(props) {
 						<Col className="card-col" xs="12" sm="12" md="8" lg="4" xl="4">
                         <CardImg top width="100%" src={img} alt={name} />
                         <Card body className="card-style">
-                            <CardTitle tag="h5">{name}</CardTitle>
+                            <CardTitle tag="h4">{name}</CardTitle>
                             <CardSubtitle tag="h6" className="mb-2 text-muted">{caption}</CardSubtitle>
                         <CardText>{description}</CardText>
                             <CardLink href={link} target="_blank" rel="noreferrer noopener">Visit Site</CardLink>
