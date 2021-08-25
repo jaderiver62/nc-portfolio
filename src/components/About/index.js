f9a4b0fdb417a1b8eff194a13dd5f1da8b3cf527
Nina Cummings <67253353+jaderiver62@users.noreply.github.com>
2021-08-25 12:15:44 -0500

    Update index.js

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -20,13 +20,14 @@ function About() {
 					My name is Kristina Cummings, but I use my childhood nickname Nina.
 					<br />
 					I have been coding now for several years and have only become more fascinated with time.
-					My primary goal is to continue to learn new technologies and develop my skills, and my work will reflect this.
+					My primary goal is to continue to learn new technologies and develop my skills and ultimately contribute to meaningful projects.
 					<br />
 					
 					Most recently, I completed a coding boot camp cohort at UC Berkeley Extension for Full Stack Web Development.
 					<br />
 					My plan is to transfer from City College of San Francisco to the University of Minnesota for a double major BS in Computer Science and Mathematics.
 					<br />
+					This career path is my passion right now, so I am dedicated to creating new and innovative projects in the future!
 					<br /><br />
 					Thank you for taking the time to review my work!
 					<br /><br />
